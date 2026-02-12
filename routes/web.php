c8e76c23efe22162a6f587cacbd35da00437ec3a
youness <younesshamou013@gmail.com>
2026-02-12 09:34:25 +0100

    creation des routes pour les produits

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request as HttpRequest;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
@@ -10,10 +11,19 @@ use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Http\Request;
 
 Route::prefix('admin')->name('admin.')->group(function(){
-    Route::get('/post', function(){
-        return 'posts';
-    });
+    Route::prefix('products')->name('products.')->group(function(){
+        Route::get('/', [ProductController::class, 'index'])->where('id', '[0-9]+')->name('index');;
 
+        Route::get('/create' , [ProductController::class, 'create'])->name('create');
+        
+        Route::post('/create', [ProductController::class, 'store'])->name('store');});
+
+        Route::get('/{id}/edit', [ProductController::class, 'edit'])->name('edit');
+
+        Route::delete('/{id}/delete', [ProductController::class, 'destroy'])->name('destroy');
+
+
+});
     Route::prefix('posts')->name('posts.')->group(function(){
         Route::get('/', [AdminController::class, 'index'])->where('id', '[0-9]+')->name('posts.index');;
 
@@ -30,4 +40,3 @@ Route::prefix('admin')->name('admin.')->group(function(){
         });
 
     });
-});
