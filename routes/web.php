94f7e98a991a1877b73562b19871dafa7d1f2bcb
youness <younesshamou013@gmail.com>
2026-02-19 14:33:14 +0000

    auth : installing breeze

--- routes/web.php
+++ routes/web.php
@@ -1,41 +1,20 @@
 <?php
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\ProductController;
-use Illuminate\Http\Request as HttpRequest;
-use Illuminate\Support\Facades\Redirect;
+
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\View\View;
-use League\Uri\Http;
-use Symfony\Component\HttpFoundation\Response;
-use Illuminate\Http\Request;
 
-Route::prefix('admin')->name('admin.')->group(function(){
-    Route::prefix('products')->name('products.')->group(function(){
-        Route::get('/', [ProductController::class, 'index'])->where('id', '[0-9]+')->name('index');;
-
-        Route::get('/create' , [ProductController::class, 'create'])->name('create');
-        
-        Route::post('/create', [ProductController::class, 'store'])->name('store');});
-
-        Route::get('/{id}/edit', [ProductController::class, 'edit'])->name('edit');
-
-        Route::delete('/{id}/delete', [ProductController::class, 'destroy'])->name('destroy');
-
-
+Route::get('/', function () {
+    return view('welcome');
 });
-    Route::prefix('posts')->name('posts.')->group(function(){
-        Route::get('/', [AdminController::class, 'index'])->where('id', '[0-9]+')->name('posts.index');;
 
-        Route::get('/create', function(){
-            return view('createPost');
-        });
-        
-        Route::post('/create',function(Request $request){
-            return "Données reçues: " . $request->input('name');
-        });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-        Route::get('/{id}/edit', function($id,Request $request){
-            return view('createPost');
-        });
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-    });
+require __DIR__.'/auth.php';
