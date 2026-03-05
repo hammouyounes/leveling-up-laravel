02c77ca5e1e6258b8d4e4bba72c025e684d93a79
youness <younesshamou013@gmail.com>
2026-03-05 11:17:58 +0000

    feat: Create Article model, controller, and migration for articles

---
+++ database/migrations/2026_03_05_111500_create_articles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     
+     */
+
+//     ✓ id
+// ✓ title (string)
+// ✓ content (text)
+// ✓ author (string)
+// ✓ timestamp
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->string('author');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
