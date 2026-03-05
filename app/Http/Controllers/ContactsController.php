b9a668df86032b1b09435c105535a67c007157be
youness <younesshamou013@gmail.com>
2026-03-05 10:36:34 +0000

    feat: Implement contact creation and validation in ContactsController

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contact;
 use Illuminate\Http\Request;
 
 class ContactsController extends Controller
 {
-    //
+    public function create(){
+        return view('contact');
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'email'=>'required|email',
+            'message'=>'required|max:500'
+        ]);
+        Contact::create($request->all());
+        return "Contact Crée avec succées";
+    }
 }
